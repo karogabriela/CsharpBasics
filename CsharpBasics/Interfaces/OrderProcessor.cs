722481e603ed65e4ad34c88edb1bae21d98f11ce
karogabriela <87209201+karogabriela@users.noreply.github.com>
2021-09-23 21:45:37 -0600

    Example using interfaces

---
+++ CsharpBasics/Interfaces/OrderProcessor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpBasics.Interfaces
+{
+    public class OrderProcessor
+    {
+        private readonly IShippingCalculator shippingCalculator;
+        public OrderProcessor(IShippingCalculator shippingCalculator)
+        {
+            this.shippingCalculator = shippingCalculator;
+        }
+
+        public void Process(Order order)
+        {
+            if (order.IsShipped)
+                throw new InvalidOperationException("This order is already processed");
+            order.Shipment = new Shipment
+            {
+                Cost = shippingCalculator.CalculateShipping(order),
+                ShippingDate = DateTime.Today.AddDays(1)
+            };
+        }
+    }
+}
---
+++ CsharpBasics/Interfaces/StoreProgram.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpBasics.Interfaces
+{
+    public class StoreProgram
+    {
+        public void ProcessOrder()
+        {
+            var orderProcessor = new OrderProcessor(new ShippingCalculator());
+            var order = new Order { Dateplaced = DateTime.Now, TotalPrice = 100f };
+            orderProcessor.Process(order);
+        }
+    }
+}
